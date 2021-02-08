794f7197381624db15ad9b0d9eb820344b766245
elder.han <lingnanlu@gmail.com>
2021-02-08 17:22:55 +0800

    dev

--- src/main/java/run/elder/Application.java
+++ src/main/java/run/elder/Application.java
@@ -3,5 +3,6 @@ package run.elder;
 public class Application {
     public static void main(String[] args) {
         System.out.println("application");
+        System.out.println("heiehi");
     }
 }
